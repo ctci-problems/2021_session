296735a384790caf7bcc5c04dd9b6ddb567c4335
ctci-problems <95592381+ctci-problems@users.noreply.github.com>
2022-01-18 21:09:03 -0800

    Update 01_07_rotate_matrix.java

--- 01_07_rotate_matrix.java
+++ 01_07_rotate_matrix.java
@@ -4,8 +4,6 @@ class Solution {
         for(int i = 0;i < matrix.length / 2;i++) {
             
             for (int j = i;j < matrix[0].length - 1 - i;j++) {
-                
-                
                 int top = matrix[i][j];
                 int right = matrix[j][matrix[0].length - 1 - i];
                 int bottom = matrix[matrix[0].length - 1 - i][matrix[0].length - 1 - j];
@@ -23,4 +21,19 @@ class Solution {
         }
     }
     
+}
+
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0;i < matrix.length/2;i++) {
+            for (int j = i;j < matrix.length - 1 - i;j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - 1 - j][i];
+                matrix[matrix.length - 1 - j][i] = matrix[matrix.length - 1 - i][matrix.length - 1 - j];
+                matrix[matrix.length - 1 - i][matrix.length - 1 - j] = matrix[j][matrix.length - 1 - i];
+                matrix[j][matrix.length - 1 - i] = temp;
+            }
+        }
+    }
 }
