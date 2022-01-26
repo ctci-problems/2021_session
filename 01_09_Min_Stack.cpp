79c1130f6bd6c3e260e121dfcbcc107ed5328097
Shaneal <37088454+sfindley899@users.noreply.github.com>
2022-01-26 07:26:56 -0800

    Create 01_09_Min_Stack.cpp

---
+++ 01_09_Min_Stack.cpp
@@ -0,0 +1,22 @@
+class MinStack {
+public:
+
+    void push(int val) {
+        if (s.size() == 0) s.push_back({val, val});
+        // otherwise we push the current min if less than value
+        else if (getMin() > val) s.push_back({val, val});
+        else s.push_back({val, getMin()});
+    }
+
+    void pop() {
+        s.pop_back();
+    }
+
+    int top() {
+        return s[s.size() - 1].first;
+    }
+
+    int getMin() {
+        return s[s.size() - 1].second;
+    }
+};
