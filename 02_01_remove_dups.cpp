3e97a20e65d8b4d2a8d922d8f6505a8fadb2d22c
Shaneal <37088454+sfindley899@users.noreply.github.com>
2022-02-01 21:24:40 -0800

    Create 02_01_remove_dups.cpp

---
+++ 02_01_remove_dups.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+	  public:
+	  ListNode * removeDuplicates(ListNode *head)
+	  {
+        auto * current = head;
+        if (current != nullptr)
+        {
+            set<int> duplicateNodes;
+
+            duplicateNodes.insert(current->val);
+
+            while(current->next != nullptr)
+            {
+                if(duplicateNodes.find(current->next->val) != duplicateNodes.end())
+                {
+                    current->next = current->next->next;
+                }
+                else
+                {
+                    duplicateNodes.insert(current->next->val);
+                    current = current->next;
+                }
+			      }
+        }
+      
+        return head;
+    }
+};
