f1e2b41430911ea6f8ea57d186ebc031c47734a4
Shaneal Findley <37088454+sfindley899@users.noreply.github.com>
2022-03-13 12:49:46 -0700

    Create 04_08_First_Common_Ancestor.cpp

---
+++ 04_08_First_Common_Ancestor.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if (root == nullptr)
+            return root;
+        
+        if ((root == p) or ((root == q))
+            return root;
+        
+        auto * left = lowestCommonAncestor(root->left, p, q);
+        auto * right = lowestCommonAncestor(root->right, p, q);
+
+        if ((left != nullptr) and (right != nullptr))
+            return root;
+
+        else if (left != nullptr)
+            return left;
+
+        else if (right != nullptr)
+            return right;
+
+        else
+            return nullptr;
+    }
+};
