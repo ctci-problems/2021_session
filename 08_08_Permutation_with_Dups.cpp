d5258af9f976e949bd2b90d3feba9bc7c6565c05
Shaneal Findley <37088454+sfindley899@users.noreply.github.com>
2022-05-12 13:27:24 -0700

    Add second solution to 8.8

--- 08_08_Permutation_with_Dups.cpp
+++ 08_08_Permutation_with_Dups.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
 public:
     void permutations_helper(vector<vector<int>> & permutations, vector<int> & nums, vector<int> current_list, unordered_map<int, int> frequencies) {
@@ -27,6 +28,35 @@ public:
             frequencies[num]++;
 
         permutations_helper(permutations, nums, vector<int>(), frequencies);
+        return permutations;
+    }
+};
+
+// Solution 2
+
+class Solution {
+public:
+    
+    void permutations_helper(vector<vector<int>> & permutations, vector<int> & nums, int current_index, set<vector<int>> & visited) {
+        if (current_index == nums.size()) {
+            if (visited.find(nums) == visited.end()) {
+                permutations.push_back(nums);
+                visited.insert(nums);   
+            }
+        } else {
+            for(int i = current_index; i < nums.size(); i++) {
+                std::swap(nums[current_index], nums[i]);
+                permutations_helper(permutations, nums, current_index + 1, visited);
+                std::swap(nums[current_index], nums[i]);
+            }
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> permutations;
+        set<vector<int>> seen;
+
+        permutations_helper(permutations, nums, 0, seen);
         return permutations;
     }
 };
